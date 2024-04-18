60f34539ed4b379ec79f3691cfd77d5fefc3b257
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-04-18 09:48:50 +0500

    3

--- src/taskTracker/tracker/service/TaskManager.java
+++ src/taskTracker/tracker/service/TaskManager.java
@@ -16,30 +16,19 @@ public interface TaskManager {
 
     ArrayList<Task> getPrioritizedTasks();
 
-
-
-
-
-
     // РАБОТА С ПРОСТЫМИ ЗАДАЧАМИ
 
     public void addTask(Task task); // добавить новую задачу в мапу
 
-
     public ArrayList<Task> getTasks(); // получить все простые задачи
-
 
     public void deleteTask();  // очистить мапу с простыми задачами
 
-
     public Task getTaskbyId(int taskID); // получить задачу по id
-
 
      public void updateTask(Task task, Task newTask); // обновить задачу новой задачей
 
-
     public void deleteTaskById(int taskID);// удалить задачу по id
-
 
     // РАБОТА С ЭПИКАМИ
 
@@ -47,31 +36,25 @@ public interface TaskManager {
 
     public ArrayList<Epic> getEpics();// Получить все Эпики
 
-
     public void deleteEpics();// очистить мапу c Эпиками, удалить все подзадачи
-
 
     public Epic getEpicbyId(int taskID);// вывести эпик по id
 
-
     public ArrayList<SubTask> getsEpicSubtasks(Epic epic);  // получить список подзадач определенного Эпика
 
-
     public void updateEpic(Epic epic, Epic newEpic); // обновить Эпик новым Эпиком
-
 
     public boolean deleteEpicById(int taskID); // удалить Эпик по id,а так же его подзадачи
 
     public void computeEpicStatus(Epic epic);
+
     public void computeEpicDataTime (Epic epic, SubTask subTask);
 
 //ПОДЗАДАЧИ
 
     public void addSubTask(SubTask subTask);// добавить новую подзадачу.
 
-
     public ArrayList<SubTask> getSubTasks();
-
 
     public void deleteSubTask(); // очистить мапу с подзадачами, удалить подазадачи из эпиков и обновить их статусы
 
@@ -79,10 +62,10 @@ public interface TaskManager {
 
     public void updateSubTask(SubTask subTask, SubTask newSubtask);// обновляем поздачу новой подзадачей и обновить статус Епика
 
-
     public boolean deleteSubTaskById(int taskID); // удалить подзадачу по id, удалить из Эпика + обновить статус
 
      public List<Task> getHistori();
+
      public List<Task> getOldVersionHistori();
 
 }
