848afa17d75539ed1d995b3ab968de6c090e84f0
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-04-18 11:14:07 +0500

    500

--- src/service/Node.java
+++ src/service/Node.java
@@ -5,7 +5,7 @@ public class Node<T> {
     private Node<T> next;
     private Node<T> prev;
 
-    public Node(T data, Node<T> next, Node<T> prev ) {
+    public Node(T data, Node<T> next, Node<T> prev) {
         this.data = data;
         this.next = next;
         this.prev = prev;
