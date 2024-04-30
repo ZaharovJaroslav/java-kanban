459e27afe2026fe51ee86909a6afdde35e6048b5
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-04-30 23:39:05 +0500

    правка кода по результатам тестов-5

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -2,9 +2,11 @@ package service;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import server.DurationAdapter;
 import server.LocalDateTimeAdapter;
 
 import java.io.File;
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 public class Managers {
@@ -24,6 +26,8 @@ public class Managers {
     public static Gson getGson() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter());
+        gsonBuilder.registerTypeAdapter(Duration.class, new DurationAdapter());
+
         return gsonBuilder.create();
     }
 
